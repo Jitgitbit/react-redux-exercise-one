d1f50550e921f8e821fcbdd07ce491c4a6e1cdff
Jitgitbit <rowowolf@gmail.com>
2020-03-12 17:38:09 +0100

    EUREKA !! REFACTORING !! key and value  same name

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,6 +1,12 @@
 export const deletePost = (id) => {
   return {
     type: 'DELETE_POST',                               // THIS IS AN ACTIONCREATOR !!!
-    id: id
+    id                       //====>> REMEMBER: if we have property key and value with the same name we can refactor like this !!! old way under here
   }
 }
+// export const deletePost = (id) => {
+//   return {
+//     type: 'DELETE_POST',                               // THIS IS AN ACTIONCREATOR !!!
+//     id: id
+//   }
+// }
