1acd7c23331f179a55b3e735a0570ba1bace74e1
Jitgitbit <rowowolf@gmail.com>
2020-03-12 20:27:21 +0100

    note for hoc withRouter, concerning history

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import {Link, NavLink, withRouter} from 'react-router-dom';
 function NavBar(props) {
   console.log(`=========>> PROPS IN NavBar:`,props)
   // setTimeout(() => {
-  //   props.history.push('/about')
+  //   props.history.push('/about')                     // <<========= Thanks to withRouter and adding props, this now works !
   // }, 2000);
   return (
     <nav className='nav-wrapper red darken-3'>
